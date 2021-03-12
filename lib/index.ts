2886f8277fa04688443a88366319f57cc01571f0
Tenji <patricepetit@hotmail.com>
2021-03-12 20:36:08 +0100

    fix/import: add missing status and colorpicket component exports

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,7 @@
 export { default as Button } from "./components/Button/index"
 export { default as Tag } from "./components/Tag/index"
+export { default as Status } from "./components/Status/index"
+export { default as ColorPicker } from "./components/ColorPicker"
 
 export { default as ThemeContext } from "./context/themeProvider"
 export { Theme } from "./context/themeProvider"
